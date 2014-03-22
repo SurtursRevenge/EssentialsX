essentials/Essentials

Future Java versions may not support _ as identifier.
Left previous method as deprecated for current API items.

Essentials/src/com/earth2me/essentials/PlayerList.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.utils.FormatUtil;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,11 +30,11 @@ public class PlayerList
 			needComma = true;
 			if (user.isAfk())
 			{
-				groupString.append(_("listAfkTag"));
+				groupString.append(tl_("listAfkTag"));
 			}
 			if (user.isHidden())
 			{
-				groupString.append(_("listHiddenTag"));
+				groupString.append(tl_("listHiddenTag"));
 			}
 			user.setDisplayNick();
 			groupString.append(user.getDisplayName());
@@ -58,11 +58,11 @@ public class PlayerList
 		String online;
 		if (showHidden && playerHidden > 0)
 		{
-			online = _("listAmountHidden", server.getOnlinePlayers().length - playerHidden, playerHidden, server.getMaxPlayers());
+			online = tl_("listAmountHidden", server.getOnlinePlayers().length - playerHidden, playerHidden, server.getMaxPlayers());
 		}
 		else
 		{
-			online = _("listAmount", server.getOnlinePlayers().length - playerHidden, server.getMaxPlayers());
+			online = tl_("listAmount", server.getOnlinePlayers().length - playerHidden, server.getMaxPlayers());
 		}
 		return online;
 	}
@@ -132,7 +132,7 @@ public class PlayerList
 		}
 		if (users == null || users.isEmpty())
 		{
-			throw new Exception(_("groupDoesNotExist"));
+			throw new Exception(tl_("groupDoesNotExist"));
 		}
 		final StringBuilder displayGroupName = new StringBuilder();
 		displayGroupName.append(Character.toTitleCase(groupName.charAt(0)));
@@ -144,7 +144,7 @@ public class PlayerList
 	public static String outputFormat(final String group, final String message)
 	{
 		final StringBuilder outputString = new StringBuilder();
-		outputString.append(_("listGroupTag", FormatUtil.replaceFormat(group)));
+		outputString.append(tl_("listGroupTag", FormatUtil.replaceFormat(group)));
 		outputString.append(message);
 		return outputString.toString();
 	}

Essentials/src/com/earth2me/essentials/SpawnMob.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.Mob.MobException;
 import com.earth2me.essentials.utils.LocationUtil;
 import com.earth2me.essentials.utils.StringUtil;
@@ -34,7 +34,7 @@ public class SpawnMob
 		}
 		if (availableList.isEmpty())
 		{
-			availableList.add(_("none"));
+			availableList.add(tl_("none"));
 		}
 		return StringUtil.joinList(availableList);
 	}
@@ -88,7 +88,7 @@ public class SpawnMob
 		final Block block = LocationUtil.getTarget(user.getBase()).getBlock();
 		if (block == null)
 		{
-			throw new Exception(_("unableToSpawnMob"));
+			throw new Exception(tl_("unableToSpawnMob"));
 		}
 		spawnmob(ess, server, user.getSource(), user, block.getLocation(), parts, data, mobCount);
 	}
@@ -125,7 +125,7 @@ public class SpawnMob
 		if (mobCount > effectiveLimit)
 		{
 			mobCount = effectiveLimit;
-			sender.sendMessage(_("mobSpawnLimit"));
+			sender.sendMessage(tl_("mobSpawnLimit"));
 		}
 
 		Mob mob = Mob.fromName(parts.get(0)); // Get the first mob
@@ -135,19 +135,19 @@ public class SpawnMob
 			{
 				spawnMob(ess, server, sender, target, sloc, parts, data);
 			}
-			sender.sendMessage(mobCount * parts.size() + " " + mob.name.toLowerCase(Locale.ENGLISH) + mob.suffix + " " + _("spawned"));
+			sender.sendMessage(mobCount * parts.size() + " " + mob.name.toLowerCase(Locale.ENGLISH) + mob.suffix + " " + tl_("spawned"));
 		}
 		catch (MobException e1)
 		{
-			throw new Exception(_("unableToSpawnMob"), e1);
+			throw new Exception(tl_("unableToSpawnMob"), e1);
 		}
 		catch (NumberFormatException e2)
 		{
-			throw new Exception(_("numberRequired"), e2);
+			throw new Exception(tl_("numberRequired"), e2);
 		}
 		catch (NullPointerException np)
 		{
-			throw new Exception(_("soloMob"), np);
+			throw new Exception(tl_("soloMob"), np);
 		}
 	}
 
@@ -194,17 +194,17 @@ public class SpawnMob
 	{
 		if (mob == null)
 		{
-			throw new Exception(_("invalidMob"));
+			throw new Exception(tl_("invalidMob"));
 		}
 
 		if (ess.getSettings().getProtectPreventSpawn(mob.getType().toString().toLowerCase(Locale.ENGLISH)))
 		{
-			throw new Exception(_("disabledToSpawnMob"));
+			throw new Exception(tl_("disabledToSpawnMob"));
 		}
 
 		if (sender.isPlayer() && !ess.getUser(sender.getPlayer()).isAuthorized("essentials.spawnmob." + mob.name.toLowerCase(Locale.ENGLISH)))
 		{
-			throw new Exception(_("noPermToSpawnMob"));
+			throw new Exception(tl_("noPermToSpawnMob"));
 		}
 	}
 
@@ -214,7 +214,7 @@ public class SpawnMob
 
 		if (data.isEmpty())
 		{
-			sender.sendMessage(_("mobDataList", StringUtil.joinList(MobData.getValidHelp(spawned))));
+			sender.sendMessage(tl_("mobDataList", StringUtil.joinList(MobData.getValidHelp(spawned))));
 		}
 
 		if (spawned instanceof Zombie || type == EntityType.SKELETON)

Essentials/src/com/earth2me/essentials/commands/Commandclearinventory.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import java.util.ArrayList;
@@ -46,7 +46,7 @@ public class Commandclearinventory extends EssentialsCommand
 
 		if (allowAll && args.length > 0 && args[0].contentEquals("*"))
 		{
-			sender.sendMessage(_("inventoryClearingFromAll"));
+			sender.sendMessage(tl_("inventoryClearingFromAll"));
 			offset = 1;
 			players = Arrays.asList(server.getOnlinePlayers());
 		}
@@ -103,7 +103,7 @@ public class Commandclearinventory extends EssentialsCommand
 		{
 			if (showExtended)
 			{
-				sender.sendMessage(_("inventoryClearingAllItems", player.getDisplayName()));
+				sender.sendMessage(tl_("inventoryClearingAllItems", player.getDisplayName()));
 			}
 			player.getInventory().clear();
 		}
@@ -111,7 +111,7 @@ public class Commandclearinventory extends EssentialsCommand
 		{
 			if (showExtended)
 			{
-				sender.sendMessage(_("inventoryClearingAllArmor", player.getDisplayName()));
+				sender.sendMessage(tl_("inventoryClearingAllArmor", player.getDisplayName()));
 			}
 			player.getInventory().clear();
 			player.getInventory().setArmorContents(null);
@@ -123,7 +123,7 @@ public class Commandclearinventory extends EssentialsCommand
 				ItemStack stack = new ItemStack(type);
 				if (showExtended)
 				{
-					sender.sendMessage(_("inventoryClearingAllStack", stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl_("inventoryClearingAllStack", stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 				}
 				player.getInventory().clear(type, data);
 			}
@@ -134,7 +134,7 @@ public class Commandclearinventory extends EssentialsCommand
 				final int removedAmount = (BASE_AMOUNT - removedStack.getAmount());
 				if (removedAmount > 0 || showExtended)
 				{
-					sender.sendMessage(_("inventoryClearingStack", removedAmount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl_("inventoryClearingStack", removedAmount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 				}
 			}
 			else
@@ -146,14 +146,14 @@ public class Commandclearinventory extends EssentialsCommand
 				ItemStack stack = new ItemStack(type, amount, data);
 				if (player.getInventory().containsAtLeast(stack, amount))
 				{
-					sender.sendMessage(_("inventoryClearingStack", amount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl_("inventoryClearingStack", amount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 					player.getInventory().removeItem(stack);
 				}
 				else
 				{
 					if (showExtended)
 					{
-						sender.sendMessage(_("inventoryClearFail", player.getDisplayName(), amount, stack.getType().toString().toLowerCase(Locale.ENGLISH)));
+						sender.sendMessage(tl_("inventoryClearFail", player.getDisplayName(), amount, stack.getType().toString().toLowerCase(Locale.ENGLISH)));
 					}
 				}
 			}

Essentials/src/com/earth2me/essentials/commands/Commandext.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -31,7 +31,7 @@ public class Commandext extends EssentialsLoopCommand
 		if (args.length < 1)
 		{
 			extPlayer(user.getBase());
-			user.sendMessage(_("extinguish"));
+			user.sendMessage(tl_("extinguish"));
 			return;
 		}
 
@@ -42,7 +42,7 @@ public class Commandext extends EssentialsLoopCommand
 	protected void updatePlayer(final Server server, final CommandSource sender, final User player, final String[] args)
 	{
 		extPlayer(player.getBase());
-		sender.sendMessage(_("extinguishOthers", player.getDisplayName()));
+		sender.sendMessage(tl_("extinguishOthers", player.getDisplayName()));
 	}
 
 	private void extPlayer(final Player player)

Essentials/src/com/earth2me/essentials/commands/Commandsethome.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials.commands;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import java.util.Locale;
@@ -54,10 +54,10 @@ public class Commandsethome extends EssentialsCommand
 		}
 		if ("bed".equals(name) || NumberUtil.isInt(name))
 		{
-			throw new NoSuchFieldException(_("invalidHomeName"));
+			throw new NoSuchFieldException(tl_("invalidHomeName"));
 		}
 		usersHome.setHome(name, location);
-		user.sendMessage(_("homeSet", user.getLocation().getWorld().getName(), user.getLocation().getBlockX(), user.getLocation().getBlockY(), user.getLocation().getBlockZ(), name));
+		user.sendMessage(tl_("homeSet", user.getLocation().getWorld().getName(), user.getLocation().getBlockX(), user.getLocation().getBlockY(), user.getLocation().getBlockZ(), name));
 
 	}
 
@@ -72,7 +72,7 @@ public class Commandsethome extends EssentialsCommand
 			}
 			if (usersHome.getHomes().size() >= limit)
 			{
-				throw new Exception(_("maxHomes", ess.getSettings().getHomeLimit(user)));
+				throw new Exception(tl_("maxHomes", ess.getSettings().getHomeLimit(user)));
 			}
 			if (limit == 1)
 			{

Essentials/src/com/earth2me/essentials/commands/Commandvanish.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
 
@@ -53,15 +53,15 @@ public class Commandvanish extends EssentialsToggleCommand
 		}
 
 		user.setVanished(enabled);
-		user.sendMessage(_("vanish", user.getDisplayName(), enabled ? _("enabled") : _("disabled")));
+		user.sendMessage(tl_("vanish", user.getDisplayName(), enabled ? tl_("enabled") : tl_("disabled")));
 
 		if (enabled == true)
 		{
-			user.sendMessage(_("vanished"));
+			user.sendMessage(tl_("vanished"));
 		}
 		if (!sender.isPlayer() || !sender.getPlayer().equals(user.getBase()))
 		{
-			sender.sendMessage(_("vanish", user.getDisplayName(), enabled ? _("enabled") : _("disabled")));
+			sender.sendMessage(tl_("vanish", user.getDisplayName(), enabled ? tl_("enabled") : tl_("disabled")));
 		}
 	}
 }

Essentials/src/com/earth2me/essentials/signs/SignBalance.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials.signs;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl_;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import net.ess3.api.IEssentials;
@@ -16,7 +16,7 @@ public class SignBalance extends EssentialsSign
 	@Override
 	protected boolean onSignInteract(final ISign sign, final User player, final String username, final IEssentials ess) throws SignException
 	{
-		player.sendMessage(_("balance", NumberUtil.displayCurrency(player.getMoney(), ess)));
+		player.sendMessage(tl_("balance", NumberUtil.displayCurrency(player.getMoney(), ess)));
 		return true;
 	}
 }

SurtursRevenge/EssentialsX

Essentials/src/com/earth2me/essentials/PlayerList.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.utils.FormatUtil;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,11 +30,11 @@ public class PlayerList
 			needComma = true;
 			if (user.isAfk())
 			{
-				groupString.append(_("listAfkTag"));
+				groupString.append(tl("listAfkTag"));
 			}
 			if (user.isHidden())
 			{
-				groupString.append(_("listHiddenTag"));
+				groupString.append(tl("listHiddenTag"));
 			}
 			user.setDisplayNick();
 			groupString.append(user.getDisplayName());
@@ -58,11 +58,11 @@ public class PlayerList
 		String online;
 		if (showHidden && playerHidden > 0)
 		{
-			online = _("listAmountHidden", server.getOnlinePlayers().length - playerHidden, playerHidden, server.getMaxPlayers());
+			online = tl("listAmountHidden", server.getOnlinePlayers().length - playerHidden, playerHidden, server.getMaxPlayers());
 		}
 		else
 		{
-			online = _("listAmount", server.getOnlinePlayers().length - playerHidden, server.getMaxPlayers());
+			online = tl("listAmount", server.getOnlinePlayers().length - playerHidden, server.getMaxPlayers());
 		}
 		return online;
 	}
@@ -132,7 +132,7 @@ public class PlayerList
 		}
 		if (users == null || users.isEmpty())
 		{
-			throw new Exception(_("groupDoesNotExist"));
+			throw new Exception(tl("groupDoesNotExist"));
 		}
 		final StringBuilder displayGroupName = new StringBuilder();
 		displayGroupName.append(Character.toTitleCase(groupName.charAt(0)));
@@ -144,7 +144,7 @@ public class PlayerList
 	public static String outputFormat(final String group, final String message)
 	{
 		final StringBuilder outputString = new StringBuilder();
-		outputString.append(_("listGroupTag", FormatUtil.replaceFormat(group)));
+		outputString.append(tl("listGroupTag", FormatUtil.replaceFormat(group)));
 		outputString.append(message);
 		return outputString.toString();
 	}

Essentials/src/com/earth2me/essentials/SpawnMob.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.Mob.MobException;
 import com.earth2me.essentials.utils.LocationUtil;
 import com.earth2me.essentials.utils.StringUtil;
@@ -34,7 +34,7 @@ public class SpawnMob
 		}
 		if (availableList.isEmpty())
 		{
-			availableList.add(_("none"));
+			availableList.add(tl("none"));
 		}
 		return StringUtil.joinList(availableList);
 	}
@@ -88,7 +88,7 @@ public class SpawnMob
 		final Block block = LocationUtil.getTarget(user.getBase()).getBlock();
 		if (block == null)
 		{
-			throw new Exception(_("unableToSpawnMob"));
+			throw new Exception(tl("unableToSpawnMob"));
 		}
 		spawnmob(ess, server, user.getSource(), user, block.getLocation(), parts, data, mobCount);
 	}
@@ -125,7 +125,7 @@ public class SpawnMob
 		if (mobCount > effectiveLimit)
 		{
 			mobCount = effectiveLimit;
-			sender.sendMessage(_("mobSpawnLimit"));
+			sender.sendMessage(tl("mobSpawnLimit"));
 		}
 
 		Mob mob = Mob.fromName(parts.get(0)); // Get the first mob
@@ -135,19 +135,19 @@ public class SpawnMob
 			{
 				spawnMob(ess, server, sender, target, sloc, parts, data);
 			}
-			sender.sendMessage(mobCount * parts.size() + " " + mob.name.toLowerCase(Locale.ENGLISH) + mob.suffix + " " + _("spawned"));
+			sender.sendMessage(mobCount * parts.size() + " " + mob.name.toLowerCase(Locale.ENGLISH) + mob.suffix + " " + tl("spawned"));
 		}
 		catch (MobException e1)
 		{
-			throw new Exception(_("unableToSpawnMob"), e1);
+			throw new Exception(tl("unableToSpawnMob"), e1);
 		}
 		catch (NumberFormatException e2)
 		{
-			throw new Exception(_("numberRequired"), e2);
+			throw new Exception(tl("numberRequired"), e2);
 		}
 		catch (NullPointerException np)
 		{
-			throw new Exception(_("soloMob"), np);
+			throw new Exception(tl("soloMob"), np);
 		}
 	}
 
@@ -194,17 +194,17 @@ public class SpawnMob
 	{
 		if (mob == null)
 		{
-			throw new Exception(_("invalidMob"));
+			throw new Exception(tl("invalidMob"));
 		}
 
 		if (ess.getSettings().getProtectPreventSpawn(mob.getType().toString().toLowerCase(Locale.ENGLISH)))
 		{
-			throw new Exception(_("disabledToSpawnMob"));
+			throw new Exception(tl("disabledToSpawnMob"));
 		}
 
 		if (sender.isPlayer() && !ess.getUser(sender.getPlayer()).isAuthorized("essentials.spawnmob." + mob.name.toLowerCase(Locale.ENGLISH)))
 		{
-			throw new Exception(_("noPermToSpawnMob"));
+			throw new Exception(tl("noPermToSpawnMob"));
 		}
 	}
 
@@ -214,7 +214,7 @@ public class SpawnMob
 
 		if (data.isEmpty())
 		{
-			sender.sendMessage(_("mobDataList", StringUtil.joinList(MobData.getValidHelp(spawned))));
+			sender.sendMessage(tl("mobDataList", StringUtil.joinList(MobData.getValidHelp(spawned))));
 		}
 
 		if (spawned instanceof Zombie || type == EntityType.SKELETON)

Essentials/src/com/earth2me/essentials/commands/Commandclearinventory.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import java.util.ArrayList;
@@ -46,7 +46,7 @@ public class Commandclearinventory extends EssentialsCommand
 
 		if (allowAll && args.length > 0 && args[0].contentEquals("*"))
 		{
-			sender.sendMessage(_("inventoryClearingFromAll"));
+			sender.sendMessage(tl("inventoryClearingFromAll"));
 			offset = 1;
 			players = Arrays.asList(server.getOnlinePlayers());
 		}
@@ -103,7 +103,7 @@ public class Commandclearinventory extends EssentialsCommand
 		{
 			if (showExtended)
 			{
-				sender.sendMessage(_("inventoryClearingAllItems", player.getDisplayName()));
+				sender.sendMessage(tl("inventoryClearingAllItems", player.getDisplayName()));
 			}
 			player.getInventory().clear();
 		}
@@ -111,7 +111,7 @@ public class Commandclearinventory extends EssentialsCommand
 		{
 			if (showExtended)
 			{
-				sender.sendMessage(_("inventoryClearingAllArmor", player.getDisplayName()));
+				sender.sendMessage(tl("inventoryClearingAllArmor", player.getDisplayName()));
 			}
 			player.getInventory().clear();
 			player.getInventory().setArmorContents(null);
@@ -123,7 +123,7 @@ public class Commandclearinventory extends EssentialsCommand
 				ItemStack stack = new ItemStack(type);
 				if (showExtended)
 				{
-					sender.sendMessage(_("inventoryClearingAllStack", stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl("inventoryClearingAllStack", stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 				}
 				player.getInventory().clear(type, data);
 			}
@@ -134,7 +134,7 @@ public class Commandclearinventory extends EssentialsCommand
 				final int removedAmount = (BASE_AMOUNT - removedStack.getAmount());
 				if (removedAmount > 0 || showExtended)
 				{
-					sender.sendMessage(_("inventoryClearingStack", removedAmount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl("inventoryClearingStack", removedAmount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 				}
 			}
 			else
@@ -146,14 +146,14 @@ public class Commandclearinventory extends EssentialsCommand
 				ItemStack stack = new ItemStack(type, amount, data);
 				if (player.getInventory().containsAtLeast(stack, amount))
 				{
-					sender.sendMessage(_("inventoryClearingStack", amount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
+					sender.sendMessage(tl("inventoryClearingStack", amount, stack.getType().toString().toLowerCase(Locale.ENGLISH), player.getDisplayName()));
 					player.getInventory().removeItem(stack);
 				}
 				else
 				{
 					if (showExtended)
 					{
-						sender.sendMessage(_("inventoryClearFail", player.getDisplayName(), amount, stack.getType().toString().toLowerCase(Locale.ENGLISH)));
+						sender.sendMessage(tl("inventoryClearFail", player.getDisplayName(), amount, stack.getType().toString().toLowerCase(Locale.ENGLISH)));
 					}
 				}
 			}

Essentials/src/com/earth2me/essentials/commands/Commandext.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
 import org.bukkit.entity.Player;
@@ -31,7 +31,7 @@ public class Commandext extends EssentialsLoopCommand
 		if (args.length < 1)
 		{
 			extPlayer(user.getBase());
-			user.sendMessage(_("extinguish"));
+			user.sendMessage(tl("extinguish"));
 			return;
 		}
 
@@ -42,7 +42,7 @@ public class Commandext extends EssentialsLoopCommand
 	protected void updatePlayer(final Server server, final CommandSource sender, final User player, final String[] args)
 	{
 		extPlayer(player.getBase());
-		sender.sendMessage(_("extinguishOthers", player.getDisplayName()));
+		sender.sendMessage(tl("extinguishOthers", player.getDisplayName()));
 	}
 
 	private void extPlayer(final Player player)

Essentials/src/com/earth2me/essentials/commands/Commandsethome.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials.commands;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import java.util.Locale;
@@ -54,10 +54,10 @@ public class Commandsethome extends EssentialsCommand
 		}
 		if ("bed".equals(name) || NumberUtil.isInt(name))
 		{
-			throw new NoSuchFieldException(_("invalidHomeName"));
+			throw new NoSuchFieldException(tl("invalidHomeName"));
 		}
 		usersHome.setHome(name, location);
-		user.sendMessage(_("homeSet", user.getLocation().getWorld().getName(), user.getLocation().getBlockX(), user.getLocation().getBlockY(), user.getLocation().getBlockZ(), name));
+		user.sendMessage(tl("homeSet", user.getLocation().getWorld().getName(), user.getLocation().getBlockX(), user.getLocation().getBlockY(), user.getLocation().getBlockZ(), name));
 
 	}
 
@@ -72,7 +72,7 @@ public class Commandsethome extends EssentialsCommand
 			}
 			if (usersHome.getHomes().size() >= limit)
 			{
-				throw new Exception(_("maxHomes", ess.getSettings().getHomeLimit(user)));
+				throw new Exception(tl("maxHomes", ess.getSettings().getHomeLimit(user)));
 			}
 			if (limit == 1)
 			{

Essentials/src/com/earth2me/essentials/commands/Commandvanish.java
@@ -1,7 +1,7 @@
 package com.earth2me.essentials.commands;
 
 import com.earth2me.essentials.CommandSource;
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.User;
 import org.bukkit.Server;
 
@@ -53,15 +53,15 @@ public class Commandvanish extends EssentialsToggleCommand
 		}
 
 		user.setVanished(enabled);
-		user.sendMessage(_("vanish", user.getDisplayName(), enabled ? _("enabled") : _("disabled")));
+		user.sendMessage(tl("vanish", user.getDisplayName(), enabled ? tl("enabled") : tl("disabled")));
 
 		if (enabled == true)
 		{
-			user.sendMessage(_("vanished"));
+			user.sendMessage(tl("vanished"));
 		}
 		if (!sender.isPlayer() || !sender.getPlayer().equals(user.getBase()))
 		{
-			sender.sendMessage(_("vanish", user.getDisplayName(), enabled ? _("enabled") : _("disabled")));
+			sender.sendMessage(tl("vanish", user.getDisplayName(), enabled ? tl("enabled") : tl("disabled")));
 		}
 	}
 }

Essentials/src/com/earth2me/essentials/signs/SignBalance.java
@@ -1,6 +1,6 @@
 package com.earth2me.essentials.signs;
 
-import static com.earth2me.essentials.I18n._;
+import static com.earth2me.essentials.I18n.tl;
 import com.earth2me.essentials.User;
 import com.earth2me.essentials.utils.NumberUtil;
 import net.ess3.api.IEssentials;
@@ -16,7 +16,7 @@ public class SignBalance extends EssentialsSign
 	@Override
 	protected boolean onSignInteract(final ISign sign, final User player, final String username, final IEssentials ess) throws SignException
 	{
-		player.sendMessage(_("balance", NumberUtil.displayCurrency(player.getMoney(), ess)));
+		player.sendMessage(tl("balance", NumberUtil.displayCurrency(player.getMoney(), ess)));
 		return true;
 	}
 }